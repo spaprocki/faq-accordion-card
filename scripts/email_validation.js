7fc907fe3d78950bdcf5858232fb43de0639ed34
Szymon Paprocki <s.paprocki.vs@gmail.com>
2022-11-06 23:07:18 +0100

    Improved accessibility

--- scripts/email_validation.js
+++ scripts/email_validation.js
@@ -1,35 +1,20 @@
-const emailInputElement = document.getElementById('email-input');
-const submitButtonElement = document.getElementById('submit-button');
-const errorMessageElement = document.getElementById('error-message');
-const errorIconElement = document.getElementById('error-icon');
-
-submitButtonElement.addEventListener('click', evaluateEmail);
-
-function evaluateEmail() {
-  if (!emailIsValid(emailInputElement)) {
-    showError();
-  } else {
-    clearError();
-  }
-}
-
-function emailIsValid(emailInput) {
-  const emailInputText = emailInput.value;
-  const emailRegex = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-
-  if (emailInputText.match(emailRegex)) {
-    return true;
-  } else return false;
-}
-
-function showError() {
-  errorMessageElement.classList.add('visible');
-  errorIconElement.classList.add('visible');
-  emailInputElement.classList.add('error');
-}
-
-function clearError() {
-  errorMessageElement.classList.remove('visible');
-  errorIconElement.classList.remove('visible');
-  emailInputElement.classList.remove('error');
-}
+const accordionHeaders = document.querySelectorAll('[data-accordion-header]');
+Array.prototype.forEach.call(accordionHeaders, (accordionHeader) => {
+  let target = accordionHeader.parentElement.nextElementSibling;
+  accordionHeader.onclick = () => {
+    console.log('clicked');
+    let expanded =
+      accordionHeader.getAttribute('aria-expanded') === 'true' || false;
+    accordionHeader.setAttribute('aria-expanded', !expanded);
+    if (!expanded) {
+      target.hidden = expanded;
+      setTimeout(() => target.firstElementChild.classList.add('show'), 10);
+    } else {
+      target.firstElementChild.classList.remove('show');
+      setTimeout(() => (target.hidden = expanded), 501);
+    }
+    /*accordionHeader.setAttribute('aria-expanded', !expanded);
+    target.hidden = expanded;
+    setTimeout(() => target.firstElementChild.classList.toggle('show'), 10);*/
+  };
+});
