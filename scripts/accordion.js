714559b675da5d664dae2a665faf5e24b72a3bb7
Szymon Paprocki <s.paprocki.vs@gmail.com>
2022-11-09 21:03:03 +0100

    Styles refactored

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,8 +1,8 @@
 const accordionHeaders = document.querySelectorAll('[data-accordion-header]');
 Array.prototype.forEach.call(accordionHeaders, (accordionHeader) => {
   let target = accordionHeader.parentElement.nextElementSibling;
+
   accordionHeader.onclick = () => {
-    console.log('clicked');
     let expanded =
       accordionHeader.getAttribute('aria-expanded') === 'true' || false;
     accordionHeader.setAttribute('aria-expanded', !expanded);
